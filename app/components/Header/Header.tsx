dda5fb16598d04fa2a1e40df2f1d1590e4c864ca
dawlishwarren <dawlishwarren@gmail.com>
2023-08-23 12:50:16 +0100

    responsive header added with window useEffect hook

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -1,45 +1,106 @@
 "use client";
 
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import ThemeChanger from "./ThemeChanger/ThemeChanger";
 import styles from "./header.module.css";
 import Link from "next/link";
+interface WindowType {
+	width: number;
+	height: number;
+}
+import { usePathname } from "next/navigation";
+import navStyles from "@/app/styles/utilities/navItem.module.css";
 
 export default function Header({ children }: { children: ReactNode }) {
+	const size = useWindowSize();
+	const pathname = usePathname();
+	function useWindowSize() {
+		const [windowSize, setWindowSize] = useState<WindowType>({
+			width: 0,
+			height: 0,
+		});
+		useEffect(() => {
+			function handleResize() {
+				setWindowSize({
+					width: window.innerWidth,
+					height: window.innerHeight,
+				});
+			}
+			window.addEventListener("resize", handleResize);
+			handleResize();
+			return () => window.removeEventListener("resize", handleResize);
+		}, []);
+		return windowSize;
+	}
 	const [hamburgerActive, setHamburgerActive] = useState(false);
 	const handleClick = () => {
 		setHamburgerActive(!hamburgerActive);
 	};
 	return (
 		<>
-			<header className={styles.header}>
-				<div className={styles.hamburger_icon_container}>
-					<button className={styles.hamburger_icon} onClick={handleClick}>
-						<h5 hidden>Navigation Menu</h5>
-						<span className={styles.hamburger_line}></span>
-						<span className={styles.hamburger_line}></span>
-						<span className={styles.hamburger_line}></span>
-					</button>
-				</div>
-				<div className={styles.theme_changer_container}>
-					<ThemeChanger />
-				</div>
-				{children}
-			</header>
-			<nav style={{ display: hamburgerActive ? "block" : "none" }}>
-				<ul className={styles.navigation_links}>
-					<li className={styles.navigation_link_wrapper}>
-						<Link href="/">
-							<h5>Home</h5>
-						</Link>
-					</li>
-					<li className={styles.navigation_link_wrapper}>
-						<Link href="/directory">
-							<h5>Directory</h5>
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			{size.width >= 1024 ? (
+				<header className={styles.header}>
+					<nav role="navigation">
+						<div>
+							<Link tabIndex={0} href="/">
+								<h5
+									className={
+										pathname == "/" ? navStyles.disabled : navStyles.nav_item
+									}>
+									Home
+								</h5>
+							</Link>
+							<Link href="/directory">
+								<h5
+									className={
+										pathname == "/directory"
+											? navStyles.disabled
+											: navStyles.nav_item
+									}>
+									Directory
+								</h5>
+							</Link>
+						</div>
+					</nav>
+					<div className={styles.theme_changer_container}>
+						<ThemeChanger />
+					</div>
+					<nav className={`${styles.nav} ${styles.align_right}`} id="authLinks">
+						{children}
+					</nav>
+				</header>
+			) : (
+				<>
+					<header className={styles.header}>
+						<div className={styles.hamburger_icon_container}>
+							<button className={styles.hamburger_icon} onClick={handleClick}>
+								<h5 hidden>Navigation Menu</h5>
+								<span className={styles.hamburger_line}></span>
+								<span className={styles.hamburger_line}></span>
+								<span className={styles.hamburger_line}></span>
+							</button>
+						</div>
+						<div className={styles.theme_changer_container}>
+							<ThemeChanger />
+						</div>
+						{children}
+					</header>
+					<nav style={{ display: hamburgerActive ? "block" : "none" }}>
+						<ul className={styles.navigation_links}>
+							<li className={styles.navigation_link_wrapper}>
+								<Link href="/">
+									<h5>Home</h5>
+								</Link>
+							</li>
+							<li className={styles.navigation_link_wrapper}>
+								<Link href="/directory">
+									<h5>Directory</h5>
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</>
+			)}
 		</>
 	);
 }
--- app/components/Header/ThemeChanger/ThemeChanger.tsx
+++ app/components/Header/ThemeChanger/ThemeChanger.tsx
@@ -1,10 +1,11 @@
-'use client';
-import Image from 'next/image';
-import themeChange from '../../../../public/dark-theme-svgrepo-com.svg';
-import styles from '../header.module.css';
-import buttonStyles from '../../../styles/utilities/button.module.css';
-import { useRef } from 'react';
-import { useTheme } from 'next-themes';
+"use client";
+import Image from "next/image";
+import themeChange from "../../../../public/dark-theme-svgrepo-com.svg";
+import styles from "../header.module.css";
+import buttonStyles from "../../../styles/utilities/button.module.css";
+import { useRef } from "react";
+import { useTheme } from "next-themes";
+import dialogStyles from "@/app/styles/utilities/dialog.module.css";
 
 export default function ThemeChanger() {
 	const dialogRef = useRef<any>(null);
@@ -25,39 +26,42 @@ export default function ThemeChanger() {
 				<Image
 					src={themeChange}
 					width={50}
-					style={{ objectFit: 'contain' }}
-					alt='Light/Dark Mode Theme Change'
+					style={{ objectFit: "contain" }}
+					alt="Light/Dark Mode Theme Change"
 				/>
 			</button>
-			<dialog ref={dialogRef}>
-				<div className={styles.dialog_content}>
-					<div className={styles.options}>
+			<dialog className={dialogStyles.dialog} ref={dialogRef}>
+				<div className={dialogStyles.dialog_content}>
+					<div className={dialogStyles.options}>
 						<button
-							className={`${buttonStyles.button_large} button`}
-							value='system'
+							className={`${buttonStyles.button_large}`}
+							value="system"
 							onClick={handleClick}
 							onMouseDown={handleClick}>
 							System
 						</button>
-
 						<button
-							className={`${buttonStyles.button_large} button`}
-							value='light'
+							className={`${buttonStyles.button_large}`}
+							value="light"
 							onClick={handleClick}
 							onMouseDown={handleClick}>
 							Light
 						</button>
 						<button
-							className={`${buttonStyles.button_large} button`}
-							value='dark'
+							className={`${buttonStyles.button_large}`}
+							value="dark"
 							onClick={handleClick}
 							onMouseDown={handleClick}>
 							Dark
 						</button>
 					</div>
-					<button className={buttonStyles.button_medium} onClick={closeModal}>
-						Close
-					</button>
+					<div className={dialogStyles.close_button_wrapper}>
+						<button
+							className={`${buttonStyles.button_medium} ${dialogStyles.close_button}`}
+							onClick={closeModal}>
+							Close
+						</button>
+					</div>
 				</div>
 			</dialog>
 		</>
